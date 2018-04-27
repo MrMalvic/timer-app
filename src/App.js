93768aa61289c4a6703669b42a5fc1727a1b40df
HackersGuild <chandacharles>
2018-04-27 08:54:55 +0200

    shared timer-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Timer} from './timer.jsx';
+import './App.css';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Our Timer-Application</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
